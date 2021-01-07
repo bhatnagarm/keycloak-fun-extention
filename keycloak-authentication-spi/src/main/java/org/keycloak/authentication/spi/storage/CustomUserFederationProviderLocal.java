f7e4d35c9c75975927ab03957c821c50aade1a49
bhatnagarm <mitul_manik@yahoo.com>
2021-01-07 13:45:26 +1000

    feat(develop): adding some code to authentication-spi.

---
+++ keycloak-authentication-spi/src/main/java/org/keycloak/authentication/spi/storage/CustomUserFederationProviderLocal.java
@@ -0,0 +1,16 @@
+package org.keycloak.authentication.spi.storage;
+
+import org.keycloak.component.ComponentModel;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.storage.UserStorageProvider;
+import org.keycloak.storage.user.UserLookupProvider;
+import org.keycloak.storage.user.UserQueryProvider;
+
+public interface CustomUserFederationProviderLocal extends UserStorageProvider,
+        UserLookupProvider,
+        UserQueryProvider {
+
+    void setModel(final ComponentModel model);
+
+    void setSession(final KeycloakSession session);
+}
